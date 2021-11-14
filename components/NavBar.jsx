f171216d17e21028e3537f1e0faf55c1e9496df1
narzaut <i.arzaut@itecriocuarto.org.ar>
2021-11-13 21:28:43 -0300

    burger menu working properly

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,29 +1,33 @@
 import Link from 'next/link'
 import { useState } from 'react'
-
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export const NavBar = ({ data }) => {
     const [isMenuDeployed, setIsMenuDeployed] = useState(false)
 	return(
-		<nav class="fadeInDown gap-4 flex-col md:flex-row bg-gray-800  w-full flex justify-between items-center text-gray-200 py-8 ">
-  	        <div className='w-full flex '>
-                <div className='w-full flex'>
-                    <div onClick = {() => setIsMenuDeployed(!isMenuDeployed)} className='flex w-full w-1/3 items-center justify-center'>
-                        <i class="fas fa-ellipsis-v"></i>
+		<nav class="z-10 sticky fadeInDown gap-4 flex-col md:flex-row bg-gray-800   w-full flex justify-between items-center text-gray-200 py-6  ">
+  	        <div className='w-full flex h-full relative z-10 '>
+                <div className='w-full flex px-4 z-10 '>
+                    <div onClick = {() => setIsMenuDeployed(!isMenuDeployed)} className=' flex w-full w-1/3 items-center'>
+                        <div className=''>
+                            <FontAwesomeIcon icon={faEllipsisV} className='hover:text-red-400 w-2' />
+
+                        </div>
                     </div>
                     <div className='flex w-full w-1/3 text-center items-center justify-center'>
   			        	<span class='text-4xl '>LOGO</span>
 			        </div>
-                    <div className='w-1/3 flex items-center justify-center'>
+                    <div className='w-1/3 flex items-center justify-end '>
                         <Link href='/'>
-                            <a className='outline-none border-b-2 border-transparent hover:border-green-400 rounded font-bold text-gray-200 hover:text-green-400 transition uppercase'>Contactanos</a>
+                            <a className='outline-none border-b-2 border-transparent hover:border-green-400 rounded font-bold text-gray-200 hover:text-green-400 transition uppercase'>Contacto</a>
                         </Link>
                     </div>
                 </div>
             </div>
            
             {isMenuDeployed && 
-                <div className='fadeIn'>
-                    <ul className='flex-col gap-4 flex items-center justify-center ' id='nav' >
+                <div className='fixed fadeInDown z-0 bg-gray-800 w-full absolute top-20 py-4'>
+                    <ul className='flex-col gap-4 flex items-start pl-10 justify-center ' id='nav' >
 
                            <li className=''>
                                <Link href='/'> 
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-gray-700">
       <Head>
         <title>iTec Labs - Instituto Tecnologico Rio Cuarto</title>
-        <link rel="icon" href="https://lh6.googleusercontent.com/YC87gVhr4Z2lPNmBFqsf7x5prGUq1WbF6ZLhD4-Fg3sXxhPb6g7MmXzIcGymV0aCplBuuR1Fg9c2ekNShTi3EMEkjKJWtg0" />
-        <script src="https://kit.fontawesome.com/4dac5a98fa.js" crossorigin="anonymous"></script>
-        
+        <link rel="icon" href="https://lh6.googleusercontent.com/YC87gVhr4Z2lPNmBFqsf7x5prGUq1WbF6ZLhD4-Fg3sXxhPb6g7MmXzIcGymV0aCplBuuR1Fg9c2ekNShTi3EMEkjKJWtg0" />        
       </Head>
       <NavBar />
+      <p>asd</p>
+
     </div>
   )
 }
