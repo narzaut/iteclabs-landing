ab179e4465f61285a7c413168a6cad9a54998f8c
narzaut <i.arzaut@itecriocuarto.org.ar>
2021-11-14 22:13:49 -0300

    added useWindowSize hook, conditional render service image

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,10 +1,12 @@
 import { Service } from "./Service"
+import { useWindowSize } from "../hooks/useWindowSize"
 export const Services = ({ services }) => {
+    const window = useWindowSize()
     return (
         <div className=''>
             {
                 services.map(service => {
-                    return <Service backgroundColor={ service.backgroundColor } imgSrc={ service.imgSrc } imgWidth={ service.imgWidth } title={ service.title } description={ service.description } />
+                    return <Service backgroundColor={ service.backgroundColor } imgSrc={ (service.id == 1 && window.width < 768) ? null : service.imgSrc } imgWidth={ service.imgWidth } title={ service.title } description={ service.description } />
                 })
             }
         </div>
