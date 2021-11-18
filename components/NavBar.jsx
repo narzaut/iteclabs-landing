31e4eee017a5364392564221faceb53a8d8a5ffe
i.arzaut <ignacio.arzaut@surfdevelopment.tech>
2021-11-18 19:37:07 -0300

    small css tweaks on navbar and data json

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -39,16 +39,16 @@ export const NavBar = ({  navBar }) => {
                     </div>
                     <div className='flex w-full w-1/3 lg:w-1/4  text-center items-center lg:justify-start justify-center'>
                         <img className='w-20' src={`/assets/${navBar.logoPath}`} alt="" />
-                        <p className='hidden lg:flex text-xl pl-2 font-bold  px-8'> {navBar.brandName}</p>
+                        <p className='hidden lg:flex text-2xl pl-2 font-bold  px-8'> {navBar.brandName}</p>
 			        </div>
-                    <div className='hidden lg:px-12 lg:flex items-center justify-between w-1/3 lg:w-2/4'>
+                    <div className='hidden lg:px-20 lg:flex items-center justify-between w-1/3 lg:w-2/4'>
                         {navBar.burgerMenu.values.map(option => {
                             return ( 
-                                <li className=''>
+                                <div className='no-underline'>
                                     <Link href='/'> 
                                         <a className='border-b-2 text-lg border-transparent hover:border-green-400 rounded smoothscroll font-bold text-gray-800 hover:text-green-400 transition    uppercase'>{option}</a>
                                     </Link>
-                                </li>
+                                </div>
                             )      
                         })}    
                     </div>
@@ -59,6 +59,7 @@ export const NavBar = ({  navBar }) => {
                     </div>
                 </div>
             </div>
+            {/* BURGER MENU */}
             {isMenuDeployed && 
                 <div className='lg:hidden fixed fadeInDown z-0 bg-gray-200 w-full absolute top-20 py-4'>
                     <ul className='flex-col gap-4 flex items-start pl-10 justify-center ' id='nav' >
