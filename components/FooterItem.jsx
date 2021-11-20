9c64b69996e43ef212707f4967efe932245ec4f0
i.arzaut <ignacio.arzaut@surfdevelopment.tech>
2021-11-20 05:20:13 -0300

    further imporove lighthouse statistics

--- components/FooterItem.jsx
+++ components/FooterItem.jsx
@@ -1,20 +1,26 @@
 import { FaMapMarkedAlt } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
 import { BsFillTelephoneFill } from 'react-icons/bs'
+import Link from 'next/link'
 
 export const FooterItem = ({ item }) => {
 	return (
-		<a href='#' className='hover:text-green-400 flex gap-2 md:text-lg transition items-center justify-center outline-none select-none' href={``}>
-			{
-				item.icon == 'at' ?
-					<MdEmail className='text-green-400'/>
-				: item.icon == 'map-pin' ?
-					<FaMapMarkedAlt className='text-green-400'/>
-				: item.icon == 'phone' &&
-					<BsFillTelephoneFill className='text-green-400'/>				
-			}
-			{ item.value }
-		</a>
+			<Link href='/'>
+				<a className='hover:text-green-400 flex gap-2 md:text-lg transition items-center justify-center outline-none select-none' href={``}>
+					{
+						item.icon == 'at' ?
+							<MdEmail className='text-green-400'/>
+						: item.icon == 'map-pin' ?
+
+							<FaMapMarkedAlt className='text-green-400'/>
+						: item.icon == 'phone' &&
+							<BsFillTelephoneFill className='text-green-400'/>				
+					}
+					{ item.value }
+				</a>
+			</Link>
+			
+		
 		
 	)
 }
